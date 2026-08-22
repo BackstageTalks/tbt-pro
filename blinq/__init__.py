fc5a234f63bd0260476456246bf936b6563901ce
BackstageTalks <tennisbackstagetalks@gmail.com>
2026-08-22 19:13:00 +0200

    Add files via upload

--- blinq/__init__.py
+++ blinq/__init__.py
@@ -1,4 +1,12 @@
 """BlinQ symmetric decision layer."""
+
 from .model import BLINQ_MODEL_VERSION, audit_blinq_symmetry, build_blinq_prediction
 from .service import BlinqService, build_match_features
-__all__ = ["BLINQ_MODEL_VERSION", "BlinqService", "audit_blinq_symmetry", "build_blinq_prediction", "build_match_features"]
+
+__all__ = [
+    "BLINQ_MODEL_VERSION",
+    "BlinqService",
+    "audit_blinq_symmetry",
+    "build_blinq_prediction",
+    "build_match_features",
+]
